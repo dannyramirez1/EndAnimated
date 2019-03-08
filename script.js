9c0725a05a1c48aada0d2cf18eaac0419d847e83
dannyramirez1 <37849344+dannyramirez1@users.noreply.github.com>
2019-03-08 09:43:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// el.src.match("images/lemon_slot.PNG")
+// el.src = "images/orange_slot.PNG";
+// el.src = "images/cherry_slot.PNG";
+// document.getElementById('you-win').play();
+// el.src.match("images/cherry_slot.PNG")
+// el.src = "images/lemon_slot.PNG";
+// el.src.match("images/orange_slot.PNG")
+
+// cycle images in the following order: CHERRY --> LEMON --> ORANGE
+
